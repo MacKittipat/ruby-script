c941d862e959a2a0b4206c02026fcc3e6b763d3d
mac <mac.kittipat@gmail.com>
2016-08-21 12:49:13 +0700

    Write result to csv file

--- office365-account-gen.rb
+++ office365-account-gen.rb
@@ -1,9 +1,14 @@
 #
 # $ ruby office365-account-gen.rb ~/Downloads/emp.csv
 #
+# == emp.csv ==
+# Firstname,Lastname
+#
+
 require 'csv'
 customers = CSV.read(ARGV[0])
-puts "User Name,First Name,Last Name,Display Name,Job Title,Department,Office Number,Office Phone,Mobile Phone,Fax,Address,City,State or Province,ZIP or Postal Code,Country or Region"
+content = "User Name,First Name,Last Name,Display Name,Job Title,Department,Office Number,Office Phone,Mobile Phone,Fax,Address,City,State or Province,ZIP or Postal Code,Country or Region\n"
 for customer in customers
-  puts "#{customer[0]}.#{customer[1][0]}@11street.co.th".downcase + ",#{customer[0]},#{customer[1]},#{customer[0]} #{customer[1]}, , , , , , , , , , , "
+  content += "#{customer[0]}.#{customer[1][0]}@11street.co.th".downcase + ",#{customer[0]},#{customer[1]},#{customer[0]} #{customer[1]}, , , , , , , , , , , \n"
 end
+File.write('office365_emp.csv', content)
